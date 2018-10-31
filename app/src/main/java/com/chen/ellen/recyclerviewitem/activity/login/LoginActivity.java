c767e3e4fedfc0c28ec94f4c037489259dacfe6e
Ellen2018 <1961075191@qq.com>
2018-10-31 20:27:47 +0800

    修改了目录结构

--- app/src/main/java/com/chen/ellen/recyclerviewitem/activity/login/LoginActivity.java
+++ app/src/main/java/com/chen/ellen/recyclerviewitem/activity/login/LoginActivity.java
@@ -5,7 +5,7 @@ import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
 
-import com.chen.ellen.basemvp.model.BaseActivity;
+import com.chen.ellen.basemvp.view.BaseActivity;
 import com.chen.ellen.recyclerviewitem.R;
 import com.chen.ellen.recyclerviewitem.activity.login.LoginAgreement.AgreementView;
 
--- basemvp/src/main/java/com/chen/ellen/basemvp/view/BaseActivity.java
+++ basemvp/src/main/java/com/chen/ellen/basemvp/view/BaseActivity.java
@@ -1,4 +1,4 @@
-package com.chen.ellen.basemvp.model;
+package com.chen.ellen.basemvp.view;
 
 import android.support.v7.app.AppCompatActivity;
 
