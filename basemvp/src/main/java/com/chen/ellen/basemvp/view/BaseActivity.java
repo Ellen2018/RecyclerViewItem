b7c67df41c7c83ad66b86f00e16e331142cd9b75
Ellen2018 <1961075191@qq.com>
2018-10-31 20:32:03 +0800

    修改了目录结构

--- basemvp/src/main/java/com/chen/ellen/basemvp/view/BaseActivity.java
+++ basemvp/src/main/java/com/chen/ellen/basemvp/view/BaseActivity.java
@@ -6,8 +6,7 @@ import com.chen.ellen.basemvp.presenter.BasePresenter;
 import com.chen.ellen.basemvp.view.BaseView;
 
 public class BaseActivity<P extends BasePresenter> extends AppCompatActivity implements BaseView {
-
-    //
+    
     public P p;
 
 }
